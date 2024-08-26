9505380bec8ba7759e364b22b64f855ca233e032
Miguel Luna <mlunav99@gmail.com>
2024-08-26 04:44:21 -0600

    change height in responsive

--- components/Section.js
+++ components/Section.js
@@ -5,11 +5,11 @@ export const Section = ({ id, children, title }) => {
     <section
       id={id}
       className={
-        'w-full pt-24 pb-24 mb-8 px-12 md:px-32 xl:w-8/12 xl:m-auto xl:mb-36 min-h-screen'
+        'w-full pt-24 mb:pb-24 mb-8 px-12 md:px-32 xl:w-8/12 xl:m-auto xl:mb-36 md:min-h-screen'
       }
     >
-      <div className="flex flex-row justify-center align-center my-6">
-        <h1 className="font-light text-2xl md:text-4xl mb-4 text-gray-100">
+      <div className="flex flex-row justify-center align-center md:my-6">
+        <h1 className="font-light text-2xl md:text-4xl md:mb-4 text-gray-100">
           {title}
         </h1>
       </div>
